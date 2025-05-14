7c2df00d5d2712e69e6ee0ef821b01af825e8d69
Jon Dailey <jonrdailey@gmail.com>
2025-05-14 13:54:23 -0400

    Add redis to backend

--- backend/api.py
+++ backend/api.py
@@ -1,12 +1,15 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from redis import Redis
 
+redis = Redis(host='localhost', port=6379, decode_responses=True)
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/api/message')
 def get_message():
-    return jsonify(message="Hello from Flask!")
+    count = redis.incr("counter")
+    return jsonify(message=f"Hello from Flask, {count}!")
 
 if __name__ == '__main__':
     app.run(debug=True)
