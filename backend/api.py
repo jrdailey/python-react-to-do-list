ae9cdaf17d44d6c35e7ab87d24a628f66121f7d1
Jon Dailey <jonrdailey@gmail.com>
2025-05-18 20:33:17 -0400

    Make sure to consistently use snake case

--- backend/api.py
+++ backend/api.py
@@ -9,20 +9,20 @@ redis = Redis(host='localhost', port=6379, decode_responses=True)
 app = Flask(__name__)
 CORS(app)
 
-def getNextTaskId():
+def get_next_task_id():
     return redis.incr('task_id')
 
-def getStoredTasks():
+def get_stored_tasks():
     tasks_json = redis.get(KEY_TASKS)
 
     return json.loads(tasks_json) if tasks_json else []
 
-def setTasks(tasks):
+def set_tasks(tasks):
     redis.set(KEY_TASKS, json.dumps(tasks))
 
 @app.route('/api/tasks', methods=['GET'])
 def get_tasks():
-    tasks = getStoredTasks()
+    tasks = get_stored_tasks()
 
     return tasks, 200
 
@@ -31,12 +31,12 @@ def create_task():
     task_data = request.get_json()
     new_task = {
         **task_data,
-        'id': getNextTaskId()
+        'id': get_next_task_id()
     }
 
-    tasks = [new_task] + getStoredTasks()
+    tasks = [new_task] + get_stored_tasks()
 
-    setTasks(tasks)
+    set_tasks(tasks)
 
     return tasks, 201
 
@@ -48,22 +48,22 @@ def update_task(task_id):
         'id': int(task_id) # prevent modification of ID
     }
 
-    tasks = getStoredTasks()
+    tasks = get_stored_tasks()
 
     updated_tasks = [updated_task if task['id'] == int(task_id) else task for task in tasks]
 
-    setTasks(updated_tasks)
+    set_tasks(updated_tasks)
 
     return updated_tasks, 200
 
 @app.route('/api/tasks/<task_id>', methods=['DELETE'])
 def delete_task(task_id):
-    tasks = getStoredTasks()
-    filteredTasks = list(filter(lambda task: task['id'] != int(task_id), tasks))
+    tasks = get_stored_tasks()
+    filtered_tasks = list(filter(lambda task: task['id'] != int(task_id), tasks))
 
-    setTasks(filteredTasks)
+    set_tasks(filtered_tasks)
 
-    return filteredTasks, 200
+    return filtered_tasks, 200
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=4000)
