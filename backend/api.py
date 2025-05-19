0e31db836adef79928138dc9d08ea4c2e213ed29
Jon Dailey <jonrdailey@gmail.com>
2025-05-18 21:12:44 -0400

    Break out TaskService

--- backend/api.py
+++ backend/api.py
@@ -1,69 +1,36 @@
 from flask import Flask, request
 from flask_cors import CORS
 from redis import Redis
-import json
-
-KEY_TASKS = 'tasks'
+from services.task_service import TaskService
 
 redis = Redis(host='localhost', port=6379, decode_responses=True)
+task_service = TaskService(redis)
 app = Flask(__name__)
 CORS(app)
 
-def get_next_task_id():
-    return redis.incr('task_id')
-
-def get_stored_tasks():
-    tasks_json = redis.get(KEY_TASKS)
-
-    return json.loads(tasks_json) if tasks_json else []
-
-def set_tasks(tasks):
-    redis.set(KEY_TASKS, json.dumps(tasks))
-
 @app.route('/api/tasks', methods=['GET'])
 def get_tasks():
-    tasks = get_stored_tasks()
-
-    return tasks, 200
+    return task_service.get_tasks(), 200
 
 @app.route('/api/tasks', methods=['POST'])
 def create_task():
     task_data = request.get_json()
-    new_task = {
-        **task_data,
-        'id': get_next_task_id()
-    }
+    task_service.create_task(task_data)
 
-    tasks = [new_task] + get_stored_tasks()
-
-    set_tasks(tasks)
-
-    return tasks, 201
+    return task_service.get_tasks(), 201
 
 @app.route('/api/tasks/<task_id>', methods=['PUT'])
 def update_task(task_id):
     updated_task_data = request.get_json()
-    updated_task = {
-        **updated_task_data,
-        'id': int(task_id) # prevent modification of ID
-    }
+    task_service.update_task(task_id, updated_task_data)
 
-    tasks = get_stored_tasks()
-
-    updated_tasks = [updated_task if task['id'] == int(task_id) else task for task in tasks]
-
-    set_tasks(updated_tasks)
-
-    return updated_tasks, 200
+    return task_service.get_tasks(), 200
 
 @app.route('/api/tasks/<task_id>', methods=['DELETE'])
 def delete_task(task_id):
-    tasks = get_stored_tasks()
-    filtered_tasks = list(filter(lambda task: task['id'] != int(task_id), tasks))
+    task_service.delete_task(task_id)
 
-    set_tasks(filtered_tasks)
-
-    return filtered_tasks, 200
+    return task_service.get_tasks(), 200
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=4000)
---
+++ backend/services/task_service.py
@@ -0,0 +1,46 @@
+import json
+
+KEY_TASKS = 'tasks'
+
+class TaskService:
+    def __init__(self, redis):
+        self.redis = redis
+
+    def create_task(self, task_data):
+        new_task = {
+            **task_data,
+            'id': self.__get_next_task_id()
+        }
+
+        tasks = [new_task] + self.get_tasks()
+
+        self.__set_tasks(tasks)
+
+    def delete_task(self, task_id):
+        tasks = self.get_tasks()
+        filtered_tasks = list(filter(lambda task: task['id'] != int(task_id), tasks))
+
+        self.__set_tasks(filtered_tasks)
+
+    def get_tasks(self):
+        tasks_json = self.redis.get(KEY_TASKS)
+
+        return json.loads(tasks_json) if tasks_json else []
+
+    def update_task(self, task_id, task_data):
+        updated_task = {
+            **task_data,
+            'id': int(task_id) # prevent modification of ID
+        }
+
+        tasks = self.get_tasks()
+
+        updated_tasks = [updated_task if task['id'] == int(task_id) else task for task in tasks]
+
+        self.__set_tasks(updated_tasks)
+
+    def __get_next_task_id(self):
+        return self.redis.incr('task_id')
+
+    def __set_tasks(self, tasks):
+        self.redis.set(KEY_TASKS, json.dumps(tasks))
