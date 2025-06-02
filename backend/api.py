b0de920570e067f7d9c8f346bc84c7d81dc4769a
Jon Dailey <jonrdailey@gmail.com>
2025-06-01 20:22:58 -0400

    Handle 404 cases in api

--- backend/api.py
+++ backend/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from redis import Redis
 from services.task_service import TaskService
@@ -12,27 +12,35 @@ CORS(app)
 
 @app.route('/api/tasks', methods=['GET'])
 def get_tasks():
-    return task_service.get_tasks(), 200
+    return jsonify(task_service.get_tasks()), 200
 
 @app.route('/api/tasks', methods=['POST'])
 def create_task():
     task_data = request.get_json()
+
     task_service.create_task(task_data)
 
-    return task_service.get_tasks(), 201
+    return jsonify(task_service.get_tasks()), 201
 
 @app.route('/api/tasks/<task_id>', methods=['PUT'])
 def update_task(task_id):
     updated_task_data = request.get_json()
-    task_service.update_task(task_id, updated_task_data)
 
-    return task_service.get_tasks(), 200
+    updated = task_service.update_task(task_id, updated_task_data)
+
+    if not updated:
+        return jsonify({'error': 'Task not found'}), 404
+
+    return jsonify(task_service.get_tasks()), 200
 
 @app.route('/api/tasks/<task_id>', methods=['DELETE'])
 def delete_task(task_id):
-    task_service.delete_task(task_id)
+    deleted = task_service.delete_task(task_id)
 
-    return task_service.get_tasks(), 200
+    if not deleted:
+        return jsonify({'error': 'Task not found'}), 404
+
+    return jsonify(task_service.get_tasks()), 200
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=API_PORT)
--- backend/services/task_service.py
+++ backend/services/task_service.py
@@ -18,9 +18,13 @@ class TaskService:
 
     def delete_task(self, task_id):
         tasks = self.get_tasks()
+        if not self.__task_exists(tasks, task_id): return False
+
         filtered_tasks = list(filter(lambda task: task['id'] != int(task_id), tasks))
 
         self.__set_tasks(filtered_tasks)
+
+        return True
 
     def get_tasks(self):
         tasks_json = self.redis.get(KEY_TASKS)
@@ -28,16 +32,22 @@ class TaskService:
         return json.loads(tasks_json) if tasks_json else []
 
     def update_task(self, task_id, task_data):
+        tasks = self.get_tasks()
+        if not self.__task_exists(tasks, task_id): return False
+
         updated_task = {
             **task_data,
             'id': int(task_id) # prevent modification of ID
         }
 
-        tasks = self.get_tasks()
-
         updated_tasks = [updated_task if task['id'] == int(task_id) else task for task in tasks]
 
         self.__set_tasks(updated_tasks)
+
+        return True
+
+    def __task_exists(self, tasks, task_id) -> bool:
+        return int(task_id) in [task['id'] for task in tasks]
 
     def __get_next_task_id(self):
         return self.redis.incr('task_id')
